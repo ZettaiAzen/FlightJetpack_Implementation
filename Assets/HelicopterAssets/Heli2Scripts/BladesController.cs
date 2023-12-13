e539651544faff165216360923e1edc45ac19c43
Matthew Steven Taylor <matthew.Steven.taylor@gmail.com>
2023-12-13 23:06:06 +0800

    Completed 2 implementations
    Created implementation 1 of Helicopter with Roll, Pitch and Yaw functionality
    
    Felt it was too "janky" and difficult to control
    
    Created implementation 2 of Helicopter that simulates normal movement and tilt of helicopter with stabilization.
    
    Much smoother implementation and easier to control, but lacks rolling functionality.
    
    Added some comments to implementations, planning to add more comments
    
    Planning to add roll functionality to implementation 2 somehow but may run short of time

---
+++ Assets/HelicopterAssets/Heli2Scripts/BladesController.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BladesController : MonoBehaviour
+{
+    //Created enums to hold the main 3 axis
+    //This allows us to manually set the rotation of each rotor blade individually from the same script
+    public enum Axis
+    {
+        x, y, z
+    }
+    public Axis rotationAxis;
+
+    //This is just to set the blade speed and to invert the rotations
+    private float bladeSpeed;
+    public bool inverseRotation = false;
+    
+    private Vector3 rotation;
+    float rotateDegree;
+
+    //This creates the bladespeed rotation and sets a maximum rotation so it doesnt break itself apart (an actual bug I had :c)
+    public float BladeSpeed
+    {
+        get { return bladeSpeed; }
+        set { bladeSpeed = Mathf.Clamp(value, 0, 3000); }
+    }
+
+    //Sets the current transform angles to the vector3 rotation variable
+    private void Start()
+    {
+        rotation = transform.localEulerAngles;
+    }
+
+    
+    private void Update()
+    {
+        //Checks if the rotation is supposed to be inverse or not and updates the rotateDegree
+        if (inverseRotation)
+        {
+            rotateDegree -= bladeSpeed * Time.deltaTime;
+        }
+        else
+        {
+            rotateDegree += bladeSpeed * Time.deltaTime;
+        }
+        rotateDegree = rotateDegree % 360;
+        //Created a switch case for the different types of rotations based on the axis selected
+        switch (rotationAxis)
+        {
+            case Axis.y:
+                transform.localRotation = Quaternion.Euler(rotation.x, rotateDegree, rotation.z);
+                break;
+            case Axis.z:
+                transform.localRotation = Quaternion.Euler(rotation.x, rotation.y, rotateDegree);
+                break;
+            default:
+                transform.localRotation = Quaternion.Euler(rotateDegree, rotation.y, rotation.z);
+                break;
+        }
+    }
+}
